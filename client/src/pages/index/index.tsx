6372197d19a0acfe1f617fb7e673fa22cb31422c
limichange <limichange@hotmail.com>
2019-12-01 17:27:16 +0800

    Update

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -1,18 +1,22 @@
 import Taro, { Component, Config } from "@tarojs/taro";
-import { View, ScrollView } from "@tarojs/components";
+import { View, ScrollView, Input } from "@tarojs/components";
+import data from "./data";
 import "./index.scss";
 
-export default class Index extends Component {
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
+export default class Index extends Component<any, any> {
   config: Config = {
-    navigationBarTitleText: "首页",
+    navigationBarTitleText: "首页"
   };
+
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      searchInput: ""
+    };
+  }
+
+  data = data.roster;
 
   componentWillMount() {}
 
@@ -25,16 +29,35 @@ export default class Index extends Component {
   componentDidHide() {}
 
   onShareAppMessage() {
-    return {}
+    return {};
   }
 
-  render() {
-    const a = Array(1000).fill(1, 0, 1000);
+  searchInputInput = e => {
+    this.setState({
+      searchInput: e.detail.value
+    });
+  };
 
-    console.log(a);
+  render() {
+    let a = this.data.filter(item => item[0]);
+    const { searchInput } = this.state;
+    console.log(searchInput);
+
+    if (searchInput) {
+      a = a.filter(item => {
+        return item[0].toLocaleLowerCase().indexOf(searchInput.toLocaleLowerCase()) !== -1
+      })
+    }
 
     return (
       <View className="index">
+        <View className="search">
+          搜索
+          <Input
+            onInput={this.searchInputInput}
+            className="searchInput"
+          ></Input>
+        </View>
         <ScrollView scrollX={true}>
           <View className="table">
             <View className="item">
@@ -49,12 +72,12 @@ export default class Index extends Component {
             {a.map((item, index) => {
               return (
                 <View key={index} className="item">
-                  <View className="name">炸窝{index}</View>
-                  <View className="job">战士</View>
+                  <View className="name">{item[0]}</View>
+                  <View className="job">{item[1]}</View>
                   <View className="attendance">100%</View>
-                  <View className="EP">EP</View>
-                  <View className="GP">GP</View>
-                  <View className="PR">PR</View>
+                  <View className="EP">{item[2]}</View>
+                  <View className="GP">{item[3]}</View>
+                  <View className="PR">{item[4]}</View>
                   <View className="priority">{index}</View>
                 </View>
               );
