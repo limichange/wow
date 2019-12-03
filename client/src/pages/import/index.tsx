315381e8e7d962877457086b0934651835682774
limichange <limichange@hotmail.com>
2019-12-03 15:43:51 +0800

    Update

--- client/src/pages/import/index.tsx
+++ client/src/pages/import/index.tsx
@@ -1,18 +1,29 @@
 import { View, Textarea } from "@tarojs/components";
 import { useState } from "@tarojs/taro";
+import "./index.scss";
 
 export default function ImportPage() {
   const [data, setData] = useState([]);
-  const [textareaValue, setTextareaValue] = useState('');
+  const [textareaValue, setTextareaValue] = useState("");
+  const [textareaValueError, setTextareaValueError] = useState(false);
 
   function onTextareaUpdate(e) {
-    setTextareaValue(e.detail.value)
+    const value = e.detail.value;
+
+    setTextareaValue(e.detail.value);
+    setTextareaValueError(false);
+
+    try {
+      JSON.parse(value);
+    } catch (e) {
+      setTextareaValueError(true);
+    }
   }
 
   return (
     <View>
       <Textarea value={textareaValue} onInput={onTextareaUpdate}></Textarea>
-      import page {textareaValue}
+      {textareaValueError && <View className="error">数据格式错误</View>}
     </View>
   );
 }
