3fb58da656d5c0ec2396eacea6026eac99a3003c
limichange <limichange@hotmail.com>
2019-12-04 15:20:12 +0800

    Update

--- client/src/pages/index/Filter.tsx
+++ client/src/pages/index/Filter.tsx
@@ -2,25 +2,28 @@ import { View, Picker, Input } from "@tarojs/components";
 import { useState } from "@tarojs/taro";
 
 export default function Filter() {
-  const [pickerType, setPickerType] = useState(-1);
+  const [pickerType1, setPickerType1] = useState(0);
+  const [pickerType2, setPickerType2] = useState(0);
+  const range1 = ["EP", "GP", "PR"];
+  const range2 = ["大于", "等于", "小于"];
 
   return (
     <View className="filter">
       <Picker
-        onChange={e => setPickerType(e.detail.value)}
+        onChange={e => setPickerType1(e.detail.value)}
         mode="selector"
-        value={pickerType}
-        range={["EP", "GP", "PR"]}
+        value={pickerType1}
+        range={range1}
       >
-        {pickerType}
+        {pickerType1}
       </Picker>
       <Picker
-        onChange={() => {}}
+        onChange={e => setPickerType2(e.detail.value)}
         mode="selector"
-        value={0}
-        range={["大于", "等于", "小于"]}
+        value={pickerType2}
+        range={range2}
       >
-        大于
+        {pickerType2}
       </Picker>
       <Input type="number"></Input>
 
